4edbc3e56c7ac6afa0b351bcd04c3659b0203b37
bayyavamshi <bayyavamshi5@gmail.com>
2025-09-19 19:56:04 +0530

    First commit: added datetimemodules

---
+++ datetime_modules.py
@@ -0,0 +1,10 @@
+from datetime import datetime, date, time, timedelta
+from datetime import datetime
+
+now = datetime.now()
+# print(now.strftime('%d-%m-%y'))  # 2025-09-19 18:45:10.123456
+# print(now.strftime("%I:%M:%S"))
+# tomorrow = now + timedelta(days=1)
+print(now.year)#etc...
+# tomorrow = now + timedelta(days=1,minutes=1)
+# print(tomorrow)
